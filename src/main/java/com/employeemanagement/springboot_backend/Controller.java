85fbc23917f594090b176397c25969a38be3801a
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2025-02-22 03:01:25 +0700

    Bainding data with frontend

--- src/main/java/com/employeemanagement/springboot_backend/Controller.java
+++ src/main/java/com/employeemanagement/springboot_backend/Controller.java
@@ -2,18 +2,21 @@ package com.employeemanagement.springboot_backend;
 
 import com.employeemanagement.springboot_backend.model.Employee;
 import com.employeemanagement.springboot_backend.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/")
 public class Controller {
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
--- src/main/java/com/employeemanagement/springboot_backend/services/EmployeeService.java
+++ src/main/java/com/employeemanagement/springboot_backend/services/EmployeeService.java
@@ -2,18 +2,18 @@ package com.employeemanagement.springboot_backend.services;
 
 import com.employeemanagement.springboot_backend.model.Employee;
 import com.employeemanagement.springboot_backend.repositories.EmployeeRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
-    @Autowired
-    private EmployeeRepo employeeRepo;
+    private final EmployeeRepo employeeRepo;
 
     public List<Employee> getAllEmployees(){
-        List<Employee> employeeList = employeeRepo.findAll();
-        return employeeList;
+        return this.employeeRepo.findAll();
     }
 }
